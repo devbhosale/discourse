cba7e42dc186996628da35f44b850028b8fa1fd2
onlinedev <onlinedev@vt.edu>
2013-12-28 23:15:46 -0500

    Update list_view.js

--- app/assets/javascripts/discourse/views/list_view.js
+++ app/assets/javascripts/discourse/views/list_view.js
@@ -13,12 +13,12 @@ Discourse.ListView = Discourse.View.extend({
   
   
      $('#category-filter').on('click',  function(e){
-     $('#bi_viewby').text( 'View By | ' +  $(e.target).text() ) ;
-     $("#bi_view_ul li > a").append($('<i></i>').addClass('fa-caret-down').addClass('fa') );
+     $('#bi_viewby').first().text( 'View By | ' +  $(e.target).text() ) ;
+     $("#bi_view_ul li > a").first().append($('<i></i>').addClass('fa-caret-down').addClass('fa') );
      });
      
-     $('#bi_view_ul li > a').text( 'View By | ' + $('#category-filter li.active a', '.list-controls').text()) ;
-     $('#bi_view_ul li > a').append($('<i></i>').addClass('fa-caret-down').addClass('fa') );
+     $('#bi_view_ul li > a').first().text( 'View By | ' + $('#category-filter li.active a', '.list-controls').text()) ;
+     $('#bi_view_ul li > a').first().append($('<i></i>').addClass('fa-caret-down').addClass('fa') );
      
      
   }
