84080b8be73e508c5158eb828c9e853d91809922
onlinedev <onlinedev@vt.edu>
2013-12-28 19:34:08 -0500

    check category filter for clicks

--- app/assets/javascripts/discourse/views/list_view.js
+++ app/assets/javascripts/discourse/views/list_view.js
@@ -11,8 +11,13 @@ Discourse.ListView = Discourse.View.extend({
   
   
   didInsertElement: function() {
-    $('#bi_viewby',this).text() = 'View By | ' + $('#category-filter li.active a',this).text();
-    alert($('#bi_viewby',this).text() + "***" + $('#category-filter li.active a',this).text() )
+     this._super();
+     
+     this.$().on('click', '#category-filter', function(){
+     $('#bi_viewby',this).text() = 'View By | ' + $('#category-filter li.active a',this).text();
+     alert($('#bi_viewby',this).text() + "***" + $('#category-filter li.active a',this).text() );
+      // do stuff with jQuery
+     });
   }
 });
 
