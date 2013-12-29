0eebb297dd4a998726b32e97a4549a9b0371ae03
onlinedev <onlinedev@vt.edu>
2013-12-28 20:21:58 -0500

    remove alerts

--- app/assets/javascripts/discourse/views/list_view.js
+++ app/assets/javascripts/discourse/views/list_view.js
@@ -12,7 +12,7 @@ Discourse.ListView = Discourse.View.extend({
   didInsertElement: function() {
      
      $('#bi_viewby').text( 'View By | ' + $('#category-filter li.active a', '.list-controls').text()) ;
-     alert($('#bi_viewby','.list-controls').text() + "***" + $('#category-filter li.active a','.list-controls').text() );
+     
      
      $('#category-filter').on('click',  function(e){
      $('#bi_viewby','.list-controls').text( 'View By | ' + $('#category-filter li.active a','.list-controls').text() ) ;     
