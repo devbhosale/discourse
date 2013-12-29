8598b150ced03b672e1e50c3132df1619d067a37
onlinedev <onlinedev@vt.edu>
2013-12-28 19:16:43 -0500

    list view added to update BI custom dropdowns

--- app/assets/javascripts/discourse/views/list_view.js
+++ app/assets/javascripts/discourse/views/list_view.js
@@ -2,6 +2,7 @@
   This view handles rendering of the header of the site
 
   @class ListView
+  @auther Devdutta  added by Devdutta to update dropdown view by
   @extends Discourse.View
   @namespace Discourse
   @module Discourse
@@ -10,40 +11,7 @@ Discourse.ListView = Discourse.View.extend({
   
   
   didInsertElement: function() {
-
-    var headerView = this;
-    this.$('a[data-dropdown]').on('click.dropdown', function(e) {
-      headerView.showDropdown($(e.currentTarget));
-      return false;
-    });
-    this.$('a.unread-private-messages, a.unread-notifications, a[data-notifications]').on('click.notifications', function(e) {
-      headerView.showNotifications(e);
-      return false;
-    });
-    $(window).bind('scroll.discourse-dock', function() {
-      headerView.examineDockHeader();
-    });
-    $(document).bind('touchmove.discourse-dock', function() {
-      headerView.examineDockHeader();
-    });
-    this.examineDockHeader();
-
-    // Delegate ESC to the composer
-    $('body').on('keydown.header', function(e) {
-      // Hide dropdowns
-      if (e.which === 27) {
-        headerView.$('li').removeClass('active');
-        headerView.$('.d-dropdown').fadeOut('fast');
-      }
-      if (headerView.get('editingTopic')) {
-        if (e.which === 13) {
-          headerView.finishedEdit();
-        }
-        if (e.which === 27) {
-          return headerView.cancelEdit();
-        }
-      }
-    });
+    $('bi_viewby',this).text = 'View By | ' + $('#category-filter li.active a',this).text();
   }
 });
 
